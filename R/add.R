f20d7a9c58a7678cccb938ac5b66851b5280a7a2
Tom Palmer <remlapmot@hotmail.com>
2025-08-29 13:52:36 +0100

    Investigate plots in helpfiles

---
+++ R/add.R
@@ -0,0 +1,12 @@
+#' Add two numbers
+#' A function to add to numbers with an unrelated example.
+#' @param a Number
+#' @param b Number
+#' @export
+#' @examples
+#' #| dev: 'png'
+#' par(mar = c(5, 5, 5, 5), cex.lab = 1.3)
+#' plot(1:10, xlab = expression(hat(beta)), ylab = expression(hat(beta)))
+add <- function(a, b) {
+  a + b
+}
